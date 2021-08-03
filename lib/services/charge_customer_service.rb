e53373f57715d9503ce2b1242a2aab731a0a74ac
Alex Souza <alexnsouza6@hotmail.com>
2021-08-03 20:50:03 -0300

    feat: add execute method for charge customer service

--- lib/services/charge_customer_service.rb
+++ lib/services/charge_customer_service.rb
@@ -1,19 +1,29 @@
-class ChargeCustomerService
+require 'dry/monads'
+require_relative "../api_call/create_transaction_call"
 
-  def initialize(customer:, shipping_address:, products:, card:, http_client:)
+class ChargeCustomerService
+  include Dry::Monads[:result]
+
+  def initialize(customer:, shipping_address:, products:, credit_card:, http_client:)
     @customer = customer
     @billing_address = customer.address
     @shipping_address = shipping_address
     @products = products
-    @card = card
+    @credit_card = credit_card
     @http_client = http_client
   end
 
   def execute
-    CreateTransactionCall.new(
+    response = CreateTransactionCall.new(
       params: params,
       http_client: @http_client
     ).execute
+
+    if response[:status] === "paid"
+      Success(response)
+    else
+      Failure(response)
+    end
   end
 
   private
@@ -24,7 +34,7 @@ class ChargeCustomerService
       billing_address: @customer.address,
       shipping_address: @shipping_address,
       products: @products,
-      card: @card
+      credit_card: @credit_card
     }
   end
 end
--- spec/services/charge_customer_service_spec.rb
+++ spec/services/charge_customer_service_spec.rb
@@ -1,6 +1,51 @@
 require_relative "../../lib/services/charge_customer_service"
+require_relative "../../lib/api_call/http_client_mock"
+require_relative "../../lib/api_call/response_mock"
+require_relative "../factory_helper"
 
 RSpec.describe ChargeCustomerService do
+  let(:customer) { build(:customer) }
+  let(:shipping_address) { build(:address) }
+  let(:products) { build_list(:product, 1) }
+  let(:credit_card) { build(:credit_card) }
+  let(:http_client) { HttpClientMock.new }
+
+
   describe "#execute" do
+    context "when params are valid" do
+      subject(:charge_customer) do
+        described_class.new(
+          customer: customer,
+          shipping_address: shipping_address,
+          products: products,
+          credit_card: credit_card,
+          http_client: http_client)
+      end
+
+      it "expects to return success" do
+        response = Response.new(body: {status: "paid" })
+        http_client.mock_responses(response: response)
+
+        expect(charge_customer.execute).to be_success
+      end
+    end
+
+    context "when params are invalid" do
+      subject(:charge_customer) do
+        described_class.new(
+          customer: customer,
+          shipping_address: shipping_address,
+          products: products,
+          credit_card: credit_card,
+          http_client: http_client)
+      end
+
+      it "expects to return failure" do
+        response = Response.new(body: { status: "not paid" })
+
+        http_client.mock_responses(response: response)
+        expect(charge_customer.execute).to be_failure
+      end
+    end
   end
 end
